00253334233da69c4ff1cd92dcbac71646e5a85b
balthazar-s <schmittbalthazar@gmail.com>
2024-03-19 13:23:22 +0100

    added meny files to create menu in

---
+++ menu.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp> // Graphics library
+#include "menu.hpp"
+
+void draw_menu_closed(sf::RenderWindow window)
+{
+    return;
+}
+void draw_menu_open(sf::RenderWindow window)
+{
+    return;
+}
---
+++ menu.hpp
@@ -0,0 +1,6 @@
+#include <SFML/Graphics.hpp> // Graphics library
+
+// Menu functions
+void draw_menu_closed(sf::RenderWindow& window);
+void draw_menu_open(sf::RenderWindow& window);
+
